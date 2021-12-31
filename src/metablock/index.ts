c9eeea8bc91e5b44cea3d5b4ea2545d1dee3240e
FlandreDaisuki <vbnm123c@gmail.com>
2022-01-01 00:44:35 +0800

    Apply metablock to specific pattern filename

--- src/metablock/index.ts
+++ src/metablock/index.ts
@@ -1,18 +1,37 @@
 import type { Plugin } from 'vite';
+import { isNil } from './util';
+
+type MatchPattern = string | RegExp;
 
 export interface MetablockPluginOption {
   // input options
   file?: string;
-  matches?: string | RegExp | (string|RegExp)[];
 
   // output options
+  pattern?: MatchPattern | MatchPattern[];
   override?: Record<string, string | Record<string, string>>;
   order?: string[];
   sourcemap?: boolean;
 }
 
+const filenameFilter = (patterns: MatchPattern[]) => (filename: string): boolean => {
+  return patterns.some((pattern) => {
+    if (typeof pattern === 'string') {
+      return filename.includes(pattern);
+    } else {
+      return pattern.test(filename);
+    }
+  });
+};
 
 export default (options: MetablockPluginOption = {}): Plugin => {
+
+  const patterns = isNil(options.pattern)
+    ? [/[.]user[.]js$/]
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    : ([] as MatchPattern[]).concat(options.pattern!);
+
+  const filter = filenameFilter(patterns);
 
   return {
     name: 'vite-plugin-userscript:metablock',
@@ -22,14 +41,15 @@ export default (options: MetablockPluginOption = {}): Plugin => {
     // ref: https://rollupjs.org/guide/en/#output-generation-hooks
     generateBundle(_, bundle) {
       for (const outputChunk of Object.values(bundle)) {
-        if (outputChunk.type === 'asset') {
-          continue;
-        }
+        if (outputChunk.type === 'asset') { continue; }
+
+        const { code, fileName } = outputChunk;
+        if (!filter(fileName)) { continue; }
 
         outputChunk.code = [
           '// ==UserScript==',
           '// ==/UserScript==',
-          outputChunk.code,
+          code,
         ].join('\n');
       }
     },
---
+++ src/metablock/util.ts
@@ -0,0 +1,3 @@
+export const isNil = (value: unknown): boolean => {
+  return value === null || value === undefined;
+};
