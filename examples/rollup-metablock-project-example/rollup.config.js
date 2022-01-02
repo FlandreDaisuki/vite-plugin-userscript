4ef4a6b5e40880e1dc3cd73a8de20f9e3ac2693b
FlandreDaisuki <vbnm123c@gmail.com>
2022-01-02 14:51:14 +0800

    Add rollup-metablock-project-example

---
+++ examples/rollup-metablock-project-example/rollup.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'rollup';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+
+import { metablock } from 'vite-plugin-userscript';
+
+export default defineConfig({
+  input: 'src/main.js',
+  output: {
+    file: 'dist/out.user.js',
+    format: 'umd',
+  },
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    terser(),
+    metablock({
+      file: 'src/metablock.json',
+      override: {
+        version: '5.4.3',
+      },
+    }),
+  ],
+});
---
+++ examples/rollup-metablock-project-example/src/main.js
@@ -0,0 +1,6 @@
+import { add, add42 } from './add';
+import { sum } from 'lodash';
+
+console.log('0.1 + 0.2 =', add(0.1, 0.2));
+console.log('-42 + 42 =', add42(-42));
+console.log('sum([1, 2, 3]) =', sum([1, 2, 3]));
